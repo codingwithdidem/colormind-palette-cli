b2c862c668f9cda5440ed0d30cefa9bd645a044c
didem <didem.kucukkaraaslan@altosec.com.tr>
2022-01-10 20:13:56 +0300

    updated to v6

--- local.js
+++ local.js
@@ -1,36 +1,42 @@
-const getPalette = require('./helpers/get-palette')
-const settings = require('./helpers/default-settings')
-const hexToRgb = require('./helpers/hexToRgb')
+const getPalette = require('./helpers/get-palette');
+const settings = require('./helpers/default-settings');
+const hexToRgb = require('./helpers/hexToRgb');
 
 // Generates a complet random palette
 const random = async () => {
-    const palette = await getPalette(settings);
-    return palette;
-}
+	const palette = await getPalette(settings);
+	return palette;
+};
 
-const generatePalette = async (model = "default", colors) => {
-    model = (['ui', 'default', 'game_of_thrones', 'lego_movie', 'maple_story', 'communist'].includes(model.toLowerCase())) ? model : 'default';
+const generatePalette = async (model, colors) => {
+	model = [
+		'ui',
+		'default',
+		'game_of_thrones',
+		'lego_movie',
+		'maple_story',
+		'communist'
+	].includes(model.toLowerCase())
+		? model
+		: 'default';
 
-    let colorsRGB = colors.map(color => hexToRgb[color])
+	let colorsRGB = colors.map(color => hexToRgb(`${color}`));
 
-    while(colorsRGB.length < 5 ) {
-        colorsRGB.push("N")
-    }
+	while (colorsRGB.length < 5) {
+		colorsRGB.push('N');
+	}
 
-    const settings = {
-        model,
-        input: colorsRGB
-    }
+	const settings = {
+		model,
+		input: colorsRGB
+	};
 
-    const palette = await settings(args);
+	const palette = await getPalette(settings);
 
-    return palette;
-
-}
-
-
+	return palette;
+};
 
 module.exports = {
-    random,
-    generatePalette
-}
+	random,
+	generatePalette
+};
